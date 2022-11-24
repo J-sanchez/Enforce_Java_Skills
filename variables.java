5e64bcf1655247396fa0130a80a65f7ab2b632a4
Jonathan Sanchez <jcsanchez@Jonathans-MacBook-Pro-2.local>
2022-11-24 03:36:18 -0700

    Moving on from type int

--- variables.java
+++ variables.java
@@ -7,18 +7,34 @@ public class variables {
         int mySecondNumber = 88;
         int myThirdNumber = 10 + 5;
         int myEquation = (2 + 4) * 5;
-        
+
+        //create int variables newNum
+        int newNum = (10 + 5) + (2 * 10);
+        int newNum2 = (12 / 2);
+        int newNum3 = (6 * 8);
+
+        int newNum4 = 1000;
+
+        // create new variable myTotal for the sum of newNum(n)
+        int myTotal = newNum + newNum2 + newNum3; 
+
+        //Print first 4 variables
         System.out.println(myFirstNumber);
-       
+
         System.out.println(mySecondNumber);
 
         System.out.println(myThirdNumber);
 
         System.out.println(myEquation);
 
-    }
+        //Print the sum myTotal
+        System.out.println(myTotal);
 
-    
+        //Print the newNum4
+        System.out.println(newNum4 - myTotal + newNum3);
+
+    };
+
 }
 
 // Java keywords: List of primimtive data types  are keywords in java primitive are a special kind of built in datatypes
@@ -28,3 +44,7 @@ public class variables {
 
 // and expression is coding construct
 // by declaring a variable again, we are effectively re-declaring a variable 
+
+// Used expressions to assign values to our variables, and we used variables we created in our expressions. 
+
+// In our expression, we've looked at only on data type so far, type int

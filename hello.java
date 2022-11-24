2198b761c4208a94d38a14fb61826071d7ceb3f7
Jonathan Sanchez <jcsanchez@Jonathans-MacBook-Pro-2.local>
2022-11-24 03:17:38 -0700

    Re-enforcing Java skills

---
+++ hello.java
@@ -0,0 +1,17 @@
+public class hello {
+    public static void main(String[] args) {
+
+        //Simple call -- print hello world 
+
+        System.out.println("Hello World") ; 
+        
+        //Make a simple change to modify the print
+        System.out.println("Hello Mfer");
+
+        // Make a change to single quotes and print
+                // System.out.print('Hello world'); does not print
+        //  character literal single quotes are used for single characters and      cannot be used for string literals
+
+        //
+    }
+}
---
+++ variables.java
@@ -0,0 +1,30 @@
+public class variables {
+
+    public static void main(String[] args) {
+
+        //Print variables 
+        int myFirstNumber = 5;
+        int mySecondNumber = 88;
+        int myThirdNumber = 10 + 5;
+        int myEquation = (2 + 4) * 5;
+        
+        System.out.println(myFirstNumber);
+       
+        System.out.println(mySecondNumber);
+
+        System.out.println(myThirdNumber);
+
+        System.out.println(myEquation);
+
+    }
+
+    
+}
+
+// Java keywords: List of primimtive data types  are keywords in java primitive are a special kind of built in datatypes
+// an int is whole number without decimal values: 
+
+//Decleration statement is used to define a variable by indication the data type, and the name, then optionally to set the variable to a specific value.
+
+// and expression is coding construct
+// by declaring a variable again, we are effectively re-declaring a variable 

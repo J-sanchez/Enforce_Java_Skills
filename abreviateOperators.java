00c78bd1b63926b21947ce9f574b84e28dcafc50
Jonathan Sanchez <jcsanchez@Jonathans-MacBook-Pro-2.local>
2022-12-14 16:53:28 -0700

    Learn more short hand operators

--- abreviateOperators.java
+++ abreviateOperators.java
@@ -10,8 +10,25 @@ public class abreviateOperators {
         System.out.println(result);
         System.out.println(results);
         System.out.println(resulted);
-     }
+
+        int newResult = 5;
+        double newRequest = 5.5;
+
+        newResult = (int) (newResult + newRequest);
+        System.out.println(newResult);
+        double New = 10;
+        New *= 1.5;
+        double Newest = 10;
+        Newest /= 1.5;
+
+        System.out.println(New);
+        System.out.println(Newest);
+    }
 }
 /*
- * We can use shorthand -= or += to add and subtract. 
+ * We can use shorthand -= or += to add and subtract. Compound assignments
+ * -=
+ * +=
+ * *=
+ * /=
  */
